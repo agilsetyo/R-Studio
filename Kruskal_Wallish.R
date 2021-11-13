673c52b56ca06d0b72172ce918207a53b4203e57
agil setyo anggoro <80674737+agilsetyo@users.noreply.github.com>
2021-11-13 17:08:17 +0700

    Delete Kruskal_Wallish.R

--- Kruskal_Wallish.R
+++
@@ -1,6 +0,0 @@
-library(readxl)
-data_anova <- read_excel("C:/Users/HP PC/Downloads/1. Anova/data_anova.xlsx")
-View(data_anova)
-
-#Kruskal Wallish
-kruskal.test(Nilai ~ Perlakuan, data = data_anova)
